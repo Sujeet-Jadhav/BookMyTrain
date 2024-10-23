a320da6a583e48d72259815a933252e4bade1f32
Sujeet-Jadhav <jadhavsujeet555@gmail.com>
2024-10-23 13:50:17 +0530

    Project initialized

---
+++ backend/railway-ticket-booking/src/test/java/com/project/railway_ticket_booking/RailwayTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.railway_ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
